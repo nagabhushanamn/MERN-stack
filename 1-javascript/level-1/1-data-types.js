0279a85d48f0be2cb6d38e6fa1449592478fce78
nagabhushanam N <nagabhushanamn@gmail.com>
2020-03-10 15:02:20 +0530

    scopes

--- 1-javascript/level-1/1-data-types.js
+++ 1-javascript/level-1/1-data-types.js
@@ -6,11 +6,11 @@ console.log('-data-types-')
 console.log("---------------------------")
 
 
-
 //----------------------------------------------------------------------------------
 /**
  *
  *  data types
+ *  -----------
  *
  *  1. simple/primitives  ==> values
  *
@@ -18,10 +18,11 @@ console.log("---------------------------")
  *  b. string
  *  c. number
  *  d. boolean
+ *  e. bigint
  *
  *  2. complex/reference  ==> objects
- * 
  *
+ * 
  */
 
 //----------------------------------------------------------------------------------
@@ -39,7 +40,7 @@ var v;
 //-------------------------------------------------
 
 var name = "Nag"
-var company = 'cts'
+var company = 'cognizant'
 
 var dynamicString1 = "the trainer " + name + " teaching in " + company
 var dynamicString2 = `the trainer ${name} teaching in ${company}` // string interpolation ( es6 )
@@ -67,8 +68,6 @@ var cost = 12.12
 //-------------------------------------------------
 
 var isFinished = false
-// if (isFinished) { console.log("yes") }
-// else { console.log("not yet") }
 
 /**
  * 
@@ -81,43 +80,33 @@ var isFinished = false
  */
 
 //-------------------------------------------------
-//Q1
-//-------------------------------------------------
-
-// function getFood() {
-//     return ""
-// }
-
-// var food = getFood()
-
-// if (food) {
-//     console.log("yummy with " + food)
-// } else {
-//     console.log("oops")
-// }
-
-//-------------------------------------------------
-
 
 
 //-------------------------------------------------
+// bigint
+//-------------------------------------------------
+
+var count = 1234567890123456789012345678901234567890n
+
+
+//----------------------------------------------------------------------------------
 //complex/refence types / objects
-//-------------------------------------------------
+//----------------------------------------------------------------------------------
+
 
 /*
 
- object
+ what is object ?
 
     => data/info/attr/properties  ==> state
     => func/methods/work/oprn     ==> behav
     => addr/location/reference    ==> identity
 
-    obj ==> sbi
-
+    obj ==> s b i
 
     e,g
 
-        Trainer obj
+        trainer-obj
 
             -> state ( name , age , skill )
             -> behav ( teach )
@@ -125,76 +114,71 @@ var isFinished = false
 
     class ==> description/blueprint/template of similar objects
 
-    class  --> instance(s)
-
+    class ==> instance(s)
 
 */
 
 
 //-------------------------------------------------
-// class / constructor ( upto ES5 )
+// class / constructor ( til ES5 )
 //-------------------------------------------------
 
-// function Person(name, age) {
-//     this.name = name
-//     this.age = age
-//     // this.sayName = function () {
-//     //     console.log(`im ${this.name}`)
-//     // }
-//     // this.sayAge = function () {
-//     //     console.log(`im ${this.age} yrs old`)
-//     // }
-// }
-// Person.prototype.sayName = function () {
-//     console.log(`im ${this.name}`)
-// }
-// Person.prototype.sayAge = function () {
-//     console.log(`im ${this.age} yrs old`)
-// }
+
+function Person(name, age) {
+    this.name = name
+    this.age = age
+}
+Person.prototype.sayName = function () {
+    console.log(`im ${this.name}`)
+}
+Person.prototype.sayAge = function () {
+    console.log(`im ${this.age} yrs old`)
+}
+
 
 /**
- * 
- *  how to create object from class ?
- * 
- *  2 steps
- * 
+ *
+ *  how to create object from class?
+ *
+ *   2 steps
+ *   -------
+ *
  *   step-1: dynamic memory allocation : ==> 'new' keyword
  *   step-2: intilize obj's properties : ==> constructor
- * 
+ *
  *   syntax:
- * 
- *   var ref=new Constructor() 
- * 
+ *   -------
+ *
+ *   var ref=new Constructor()
+ *
  */
 
 
-// var p1 = new Person("Nag", 36)
-// var p2 = new Person("Indu", 31)
-// var p3 = new Person("Ria", 4)
-// var p4 = new Person("dia", 2)
-
-//
-
+var p1 = new Person("Nag", 36)
+var p2 = new Person("Indu", 31)
+var p3 = new Person("Ria", 4)
+var p4 = new Person("Dia", 2)
 
 //-------------------------------------------------
 // class / constructor ( from ES6 - 2015 )
 //-------------------------------------------------
 
-// class Person {
-//     constructor(name, age) {
-//         this.name = name
-//         this.age = age
-//     }
-//     sayName() {
-//         console.log("im " + this.name)
-//     }
-//     sayAge() {
-//         console.log("im " + this.age + " yrs old")
-//     }
-// }
 
-// var p1 = new Person("Nag", 36)
-// var p2 = new Person("Indu", 31)
+class NewPerson {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+    sayName() {
+        console.log("im " + this.name)
+    }
+    sayAge() {
+        console.log("im " + this.age + " yrs old")
+    }
+}
+
+var p1 = new NewPerson("Nag", 36)
+var p2 = new NewPerson("Indu", 31) 
 
 
 //-------------------------------------------------
@@ -216,51 +200,53 @@ var isFinished = false
 // }
 
 // var p1 = new Person("Nag", 36)
-// var p2 = new Person("Kishore", 35)
+// var p2 = new Person("Indu", 35) 
+
 
 /**
- * 
+ *
  *  imp-note :
- * 
- *  by default, all javascript objects are extensible, 
+ *
+ *  by default, all javascript objects are extensible,
  *  i.e we can add new properties on fly if required
- * 
+ *
  */
 
-// Object.preventExtensions(p1)
 // console.log(Object.isExtensible(p1))
+// Object.preventExtensions(p1)
 // p1.skill = "tech"
 
 /**
- * 
+ *
  *  imp-note :
- * 
- *  by default, all javascript objects are configurable, 
+ *
+ *  by default, all javascript objects are configurable,
  *  i.e we can delete existing properties on fly if not required
- * 
+ *
  */
 
-
-// Object.seal(p1)
 // console.log(Object.isSealed(p1))
-// p1.skill="tech"
+// Object.seal(p1)
+// p1.skill = "tech"
 // delete p1.age
 
 
 /**
- * 
+ *
  *  imp-note :
- * 
- *  by default, all javascript object's properties are modifiable, 
- *  
- * 
+ *
+ *  by default, 
+ *  all javascript object's properties are mutable
+ *
+ *
  */
 
-// Object.freeze(p1)
+
 // console.log(Object.isFrozen(p1))
-// p1.skill="tech"
+// Object.freeze(p1)
+// p1.skill = "tech"
 // delete p1.age
-// p1.age=-10
+// p1.age = -10
 
 
 // -----------------------------------------------------------------
@@ -268,26 +254,29 @@ var isFinished = false
 // -----------------------------------------------------------------
 
 /**
- * 
+ *
  *  way-1 : '.' notation
- *  way-2 : '[]' notation, if property has space or dash 
- * 
+ *  way-2 : '[]' notation, if property has space or dash
+ *
  */
 
-class Person {
-    constructor(name, age, address) {
-        this.name = name
-        this.age = age
-        this['home-address'] = address
-    }
-}
 
-var p1 = new Person("Nag", 36, "chennai")
-console.log(p1.name)
-console.log(p1['name'])
-console.log(p1.age)
-console.log(p1['age'])
-console.log(p1['home-address'])
+// class Person {
+//     constructor(name, age, address) {
+//         this.name = name
+//         this.age = age
+//         this['home-address'] = address
+//     }
+// }
+
+// var p1 = new Person("Nag", 36, "chennai")
+// console.log(p1.name)
+// console.log(p1['name'])
+// console.log(p1.age)
+// console.log(p1['age'])
+// console.log(p1['home-address']) 
+
+
 
 // -----------------------------------------------------------------
 
