85391a30e8d0917c6a5c95f75106dff64fb0d753
nagabhushanam N <nagabhushanamn@gmail.com>
2020-03-11 14:24:59 +0530

    Ex1

---
+++ 0-Ex/ex1.js
@@ -0,0 +1,32 @@
+
+// dev-1
+function f1() {
+    console.log("🐎")
+    console.log("f1()")
+    console.log("👍")
+}
+//dev-2
+function f2() {
+    console.log("🐎")
+    console.log("f2()")
+    console.log("👍")
+}
+
+
+
+// me
+//--------------------------------------
+f1()
+f2()
+//--------------------------------------
+// design issues
+//--------------------------------------
+/**
+ * 
+ *  ==> code scattering / duplication
+ *  ==> code tangling  / tight-coupling
+ * 
+ */
+// -----------------------------------------
+
+
