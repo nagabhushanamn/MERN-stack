25c69d53abf7b91466780dc264e5d162001ea4af
nagabhushanam N <nagabhushanamn@gmail.com>
2020-03-25 11:00:31 +0530

    bootstrap

---
+++ 2-web/2-CSS/example-5 ( bootstrap )/index.js
@@ -0,0 +1,26 @@
+
+
+let panel1 = document.getElementById("panel-1")
+let panel2 = document.getElementById("panel-2")
+let panel3 = document.getElementById("panel-3")
+
+panel2.style.display = "none"
+panel3.style.display = "none"
+
+function changeTab(event) {
+    if (event.target.id === "tab-1") {
+        panel1.style.display = "block"
+        panel2.style.display = "none"
+        panel3.style.display = "none"
+    }
+    if (event.target.id === "tab-2") {
+        panel1.style.display = "none"
+        panel2.style.display = "block"
+        panel3.style.display = "none"
+    }
+    if (event.target.id === "tab-3") {
+        panel1.style.display = "none"
+        panel2.style.display = "none"
+        panel3.style.display = "block"
+    }
+}
