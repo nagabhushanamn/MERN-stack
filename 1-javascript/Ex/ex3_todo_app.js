7e5fc4fbc7ce94762cae79f31c6517be1e92a5af
nagabhushanam N <nagabhushanamn@gmail.com>
2020-03-30 15:36:17 +0530

    todo app

--- 1-javascript/Ex/ex3_todo_app.js
+++ 1-javascript/Ex/ex3_todo_app.js
@@ -10,61 +10,96 @@
  * 
  */
 
- class Todo{
-     constructor(title) {
-        Todo.nextTodoIdx++
-         this.id=Todo.nextTodoIdx
-         this.title=title
-         this.completed=false
-     }
- }
- Todo.nextTodoIdx=0 // static
+class Todo {
+    constructor(title) {
+        Todo.nextTodoIdx++ // naive approach
+        this.id = Todo.nextTodoIdx
+        this.title = title
+        this.completed = false
+    }
+}
+Todo.nextTodoIdx = 0 // static
+
+
+//--------------------------------------------------
+// TodoService => usecases / services
+//--------------------------------------------------
+
+
+class TodoService {
+    constructor() {
+        this.todos = [] // array data-structure
+    }
+    addTodo(title) {
+        const newTodo = new Todo(title)
+        //this.todos.push(newTodo) // mutable
+        this.todos = this.todos.concat(newTodo) // immutable
+    }
+    editTodo(id, newTitle) {
+        //..
+        this.todos = this.todos.map(todo => {
+            if (todo.id === id) {
+                todo.title = newTitle
+            }
+            return todo
+        })
+    }
+    completeTodo(id) {
+        this.todos = this.todos.map(todo => {
+            if (todo.id === id)
+                todo.completed = !todo.completed
+            return todo
+        })
+    }
+    deleteTodo(id) {
+        // immutable
+        this.todos = this.todos.filter(todo => todo.id !== id)
+       
+        // mutate
+        // let idx=this.todos.findIndex(todo=>todo.id===id)
+        // this.todos.splice(idx,1)
+    }
+    clearCompleted() {
+        this.todos = this.todos.filter(todo => !todo.completed)
+    }
+    completeAll() {
+        let areAllCompleted = this.todos.every(todo => todo.completed)
+        this.todos = this.todos.map(todo => {
+            todo.completed = !areAllCompleted
+            return todo
+        })
+    }
+    viewTodos(flag) {
+        if (flag === "ALL") {
+            this.todos
+                .forEach(todo => console.log(todo))
+        }
+        if (flag === "ACTIVE") {
+            this.todos
+                .filter(todo => !todo.completed)
+                .forEach(todo => console.log(todo))
+        }
+        if (flag === "COMPLETED") {
+            this.todos
+                .filter(todo => todo.completed)
+                .forEach(todo => console.log(todo))
+        }
+        if (flag === "TODAY") {
+        }
+        if (flag === "WEEK") {
+        }
+        if (flag === "MONTH") {
+        }
+    }
+}
+
+
+//--------------------------------------------------
+
+const service = new TodoService()
+service.addTodo('eat')
+service.addTodo('sleep')
+service.addTodo('work')
 
  //--------------------------------------------------
- // Todo => usecases / services
- //--------------------------------------------------
-
- class TodoService{
-     constructor(){
-         this.todos=[] // data structure
-     }
-     addTodo(title){
-         const newTodo=new Todo(title)
-         this.todos=this.todos.concat(newTodo)   
-     }
-     editTodo(id,newTitle){
-         //..
-     }
-     completeTodo(id){
-         //..
-     }
-     deleteTodo(id){
-         //..
-     }
-     clearCompleted(){
-         //..
-     }
-     completeAll(){
-         //..
-     }
-     viewTodos(flag){
-         if(flag==="ALL"){
-             this.todos.forEach(todo=>console.log(todo))
-         }
-         if(flag==="ACTIVE"){
-
-         }
-         if(flag==="COPLETED"){
-
-         }
-     }
- }
-
-
- //--------------------------------------------------
-
- const service=new TodoService()
- service.addTodo('eat')
- service.addTodo('sleep')
- service.addTodo('work')
 
