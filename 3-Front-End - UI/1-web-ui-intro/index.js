82dad4615ffb06164a7faf718fd8f1adf3dc9903
nagabhushanam N <nagabhushanamn@gmail.com>
2020-03-31 11:57:06 +0530

    DOM Ready

--- 3-Front-End - UI/1-web-ui-intro/index.js
+++ 3-Front-End - UI/1-web-ui-intro/index.js
@@ -1,31 +1,34 @@
 console.log("-index.js-")
 
+// debugger
 
 //-----------------------------------------------------------
 // javascript with DOM-api ( actual html-page in browser)
 //-----------------------------------------------------------
 
+document.addEventListener('DOMContentLoaded', e => {
 
-let messageBox = document.getElementsByClassName('alert-info')[0];
-let gmBtn = document.getElementsByClassName('btn-primary')[0];
-let gnBtn = document.getElementsByClassName('btn-warning')[0];
-let geBtn = document.getElementsByClassName('btn-danger')[0];
-let reBtn = document.getElementsByClassName('btn-dark')[0];
+    let messageBox = document.getElementsByClassName('alert-info')[0];
+    let gmBtn = document.getElementsByClassName('btn-primary')[0];
+    let gnBtn = document.getElementsByClassName('btn-warning')[0];
+    let geBtn = document.getElementsByClassName('btn-danger')[0];
+    let reBtn = document.getElementsByClassName('btn-dark')[0];
 
-let originalText = ""
-gmBtn.addEventListener('click', event => {
-    originalText = messageBox.innerHTML;
-    messageBox.innerHTML = "good morning 🌺"
+    let originalText = ""
+    gmBtn.addEventListener('click', event => {
+        originalText = messageBox.innerHTML;
+        messageBox.innerHTML = "good morning 🌺"
+    })
+    gnBtn.addEventListener('click', event => {
+        messageBox.innerHTML = "good Noon 🌞"
+    })
+    geBtn.addEventListener('click', event => {
+        messageBox.innerHTML = "good evening ⭐️"
+    })
+    reBtn.addEventListener('click', event => {
+        messageBox.innerHTML = `${originalText}`
+    })
+
+
+
 })
-gnBtn.addEventListener('click', event => {
-    messageBox.innerHTML = "good Noon 🌞"
-})
-geBtn.addEventListener('click', event => {
-    messageBox.innerHTML = "good evening ⭐️"
-})
-reBtn.addEventListener('click', event => {
-    messageBox.innerHTML = originalText
-})
-
-
-
