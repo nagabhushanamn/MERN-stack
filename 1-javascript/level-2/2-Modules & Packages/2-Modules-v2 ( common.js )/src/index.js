f32833580891711273176f87f778a9984e25af44
nagabhushanam N <nagabhushanamn@gmail.com>
2020-04-13 17:36:51 +0530

    modules and packages

--- 1-javascript/level-2/2-Modules & Packages/2-Modules-v2 ( common.js )/src/index.js
+++ 1-javascript/level-2/2-Modules & Packages/2-Modules-v2 ( common.js )/src/index.js
@@ -8,7 +8,7 @@ console.log("-index.js-")
 
 
 const greet = require('intern-batch-greet')
-const lodash=require('lodash')
+const lodash = require('lodash')
 
 greet('tn')
 
@@ -19,8 +19,12 @@ greet('tn')
 
 
 let data = [1, 1, 2, 3, 4, 4, 3]
-let uniqueElements=lodash.uniq(data)
+let uniqueElements = lodash.uniq(data)
 console.log(uniqueElements)
 
+let arr1 = [1, 2, 3, 4]
+let arr2 = [5, 6, 3, 4]
+
+console.log(lodash.difference(arr1,arr2))
 
 //----------------------------------------
