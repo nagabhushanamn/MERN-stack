6c65c7e5b96e5e33145b342118311fed81702691
nagabhushanam N <nagabhushanamn@gmail.com>
2020-04-27 16:04:05 +0530

    CB

--- stage-2/1-React/4-Course-dashboard/app_v1/src/App.js
+++ stage-2/1-React/4-Course-dashboard/app_v1/src/App.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
@@ -11,14 +12,16 @@ class App extends Component {
           title: 'javascript',
           price: 100,
           description: 'best language for full-stack',
-          isAvailable: true
+          isAvailable: true,
+          log: 'images/javascript.png'
         },
         {
           id: 2,
           title: 'react.js',
           price: 200,
           description: 'best library to make UI components',
-          isAvailable: true
+          isAvailable: true,
+          log: 'images/react.png'
         }
       ]
     }
@@ -37,18 +40,24 @@ class App extends Component {
     let elements = courses.map(course => {
       return (
         <div className="list-group-item" key={course.id}>
-          <h5>{course.title}</h5>
-          <h6>&#8377;{course.price}</h6>
-          <div>{course.description}</div>
-          {/* {course.isAvailable ? <button className="btn btn-sm btn-dark">Learn</button> : null} */}
-          {this.renderLearnButton(course)}
+          <div className="row">
+            <div className="col-3">
+              <img className="img-fluid" src={course.log} alt="course logo" />
+            </div>
+            <div className="col-9">
+              <h5>{course.title}</h5>
+              <h6>&#8377;{course.price}</h6>
+              <div>{course.description}</div>
+              {/* {course.isAvailable ? <button className="btn btn-sm btn-dark">Learn</button> : null} */}
+              {this.renderLearnButton(course)}
+            </div>
+          </div>
         </div>
       )
     })
     return elements
   }
   render() {
-    let { courses } = this.state
     return (
       <div>
         <nav className="navbar navbar-light bg-info">
