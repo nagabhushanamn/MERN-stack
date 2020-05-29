2272809ae75dd31ebe4a048c8bb98cd4a290a871
Nagabhushanam <nagabhushanamn@gmail.com>
2020-05-29 11:18:45 +0530

    bus model

---
+++ Final/booking-system/api-server/initilize-db/index.js
@@ -0,0 +1,48 @@
+
+
+
+const mongoose = require('mongoose');
+const Bus = require('../model/Bus')
+
+mongoose.connect('mongodb+srv://user1:user11@cluster0-socov.mongodb.net/booking-system?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Now connected to MongoDB!'))
+    .catch(err => console.error('Something went wrong', err));
+
+
+async function insertNewBus(busNumber, busType, totalSeats) {
+    const bus = new Bus({
+        busNumber,
+        busType,
+        totalSeats
+    })
+    const result = await bus.save()
+    console.log(result)
+}
+
+// insertNewBus('KA12 G1313', 'VOLVO Multi Axle', 40)
+
+async function insertNewService(busNumber, from, to, dep, arr, fare) {
+    const bus = await Bus.findOne({ busNumber })
+    bus.service = {
+        from,
+        to,
+        dep,
+        arr,
+        fare
+    }
+    const result = await bus.save()
+    console.log(result)
+}
+
+// insertNewService('KA12 G1313', 'BENGALORE', 'CHENNAI', new Date(), new Date(), 680)
+
+
+
+async function searchServices(from, to, travelDate) {
+
+    let result = await Bus.find({ 'service.from': from, 'service.to': to,'service.dep':travelDate })
+    console.log(result)
+
+}
+
+searchServices('BENGALORE','CHENNAI',new Date())
---
+++ Final/booking-system/api-server/model/Bus.js
@@ -0,0 +1,59 @@
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+
+const reservationSchema = new Schema({
+    seats: {
+        type: Array
+    },
+    isFull: {
+        type: Boolean
+    }
+})
+
+const serviceSchema = new Schema({
+    from: {
+        type: String
+    },
+    to: {
+        type: String
+    },
+    dep: {
+        type: Date
+    },
+    arr: {
+        type: Date
+    },
+    fare: {
+        type: Number
+    }
+})
+
+const busSchema = new Schema({
+    busNumber: {
+        type: String,
+    },
+    busType: {
+        type: String
+    },
+    totalSeats: {
+        type: Number
+    },
+    seatLayout: {
+        type: String,
+        default: '4/10'
+    },
+    reservation: {
+        type: reservationSchema
+    },
+    service: {
+        type: serviceSchema
+    }
+})
+
+
+const BusModel = mongoose.model('Bus', busSchema);
+
+module.exports = BusModel;
+
